c3a7c5dccda7b5bcccaffa47d2577e7c5c8ec783
Mreza Gh <mrezagh.legend@gmail.com>
2020-08-20 00:03:43 +0430

    :recycle: state design pattern for eliminating switch  __ 20th

--- MiniJava/src/main/java/codegenerator/Address.java
+++ MiniJava/src/main/java/codegenerator/Address.java
@@ -7,28 +7,94 @@ public class Address {
     public int num;
     public TypeAddress type;
     public VarType varType;
+    private AddressMode addressMode;
 
     public Address(int num, VarType varType, TypeAddress Type) {
         this.num = num;
         this.type = Type;
+        assignAddressMode(num);
         this.varType = varType;
     }
+
 
     public Address(int num, VarType varType) {
         this.num = num;
         this.type = TypeAddress.Direct;
+        assignAddressMode(num);
         this.varType = varType;
     }
     public String toString(){
+        return addressMode.toString();
+    }
+    
+    private void assignAddressMode(int num) {
         switch (type){
             case Direct:
-                return num+"";
+                this.addressMode = new Direct(num);
             case Indirect:
-                return "@"+num;
+                this.addressMode = new InDirect(num);
             case Imidiate:
-                return "#"+num;
+                this.addressMode = new Imidiate(num);
             default:
-                return num+"";
+                this.addressMode = new Normal(num);
         }
+    }
+
+}
+
+abstract class AddressMode{
+    public int num;
+    public AddressMode(int num){
+        this.num = num;
+    }
+
+    public abstract String toString();
+}
+
+class Direct extends AddressMode{
+
+    public Direct(int num) {
+        super(num);
+    }
+
+    @Override
+    public String toString() {
+        return num+"";
+    }
+}
+
+class InDirect extends AddressMode{
+
+    public InDirect(int num) {
+        super(num);
+    }
+
+    @Override
+    public String toString() {
+        return "@"+num;
+    }
+}
+
+class Imidiate extends AddressMode{
+
+    public Imidiate(int num) {
+        super(num);
+    }
+
+    @Override
+    public String toString() {
+        return "#"+num;
+    }
+}
+
+class Normal extends AddressMode{
+
+    public Normal(int num) {
+        super(num);
+    }
+
+    @Override
+    public String toString() {
+        return num+"";
     }
 }
